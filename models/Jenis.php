56d0149080fb8399ed91495783f2ec32c09384f9
dindatry10 <dindatry10@gmail.com>
2021-05-22 12:15:06 +0700

    initial

---
+++ models/Jenis.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "jenis".
+ *
+ * @property int $id
+ * @property string $nama_jenis
+ * @property string $keterangan
+ */
+class Jenis extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'jenis';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'nama_jenis', 'keterangan'], 'required'],
+            [['id'], 'integer'],
+            [['nama_jenis', 'keterangan'], 'string', 'max' => 50],
+            [['id'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nama_jenis' => 'Nama Jenis',
+            'keterangan' => 'Keterangan',
+        ];
+    }
+}
---
+++ models/Supplier.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "supplier".
+ *
+ * @property int $id
+ * @property string $nama_supplier
+ * @property string $notelp
+ * @property string $email
+ * @property string $alamat
+ */
+class Supplier extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'supplier';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'nama_supplier', 'notelp', 'email', 'alamat'], 'required'],
+            [['id'], 'integer'],
+            [['nama_supplier'], 'string', 'max' => 50],
+            [['notelp'], 'string', 'max' => 15],
+            [['email'], 'string', 'max' => 25],
+            [['alamat'], 'string', 'max' => 100],
+            [['id'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nama_supplier' => 'Nama Supplier',
+            'notelp' => 'Notelp',
+            'email' => 'Email',
+            'alamat' => 'Alamat',
+        ];
+    }
+}
